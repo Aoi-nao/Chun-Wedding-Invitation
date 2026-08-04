a0cc2e08218ab7a8a96abfc71c3d6eeb6ce61430
Aoi-nao <aoisuzume4869@gmail.com>
2026-08-04 21:34:17 +0700

    thêm hero

--- script.js
+++ script.js
@@ -59,11 +59,17 @@ function renderHero() {
 
     if (!hero) return;
 
+    const background = hero.querySelector(".hero-background");
     const monogram = hero.querySelector(".hero-monogram");
     const image = hero.querySelector(".hero-photo-image");
     const name = hero.querySelector(".hero-name");
     const date = hero.querySelector(".hero-date");
     const quote = hero.querySelector(".hero-quote");
+
+    background.style.backgroundImage =
+        WeddingData.hero.background
+            ? `url("${WeddingData.hero.background}")`
+            : "none";
 
     monogram.textContent = WeddingData.wedding.monogram;
 
@@ -74,8 +80,7 @@ function renderHero() {
         `${WeddingData.groom.fullName}<br>&<br>${WeddingData.bride.fullName}`;
 
     date.textContent =
-        `${WeddingData.wedding.weekday}
-${WeddingData.wedding.day} • ${WeddingData.wedding.month} • ${WeddingData.wedding.year}`;
+        `${WeddingData.wedding.weekday} • ${WeddingData.wedding.day}.${WeddingData.wedding.month}.${WeddingData.wedding.year}`;
 
     quote.textContent = WeddingData.wedding.quote;
 }
