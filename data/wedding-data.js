d7b69f0a0c10ab4214c3fd6a104b4048c5250999
Aoi-nao <aoisuzume4869@gmail.com>
2026-09-20 10:30:49 +0700

    Update invitation title formatting in wedding-data.js

--- data/wedding-data.js
+++ data/wedding-data.js
@@ -53,7 +53,7 @@ const WeddingData = {
     },
 
   invitation: {
-    title: "Một ngày đặc biệt, một khoảnh khắc đáng nhớ",
+    title: "Một ngày đặc biệt,\nmột khoảnh khắc đáng nhớ",
     message: "Chúng mình rất hạnh phúc khi được chia sẻ niềm vui này cùng bạn",
     invite: "Gia đình hai bên trân trọng kính mời..."
 },
